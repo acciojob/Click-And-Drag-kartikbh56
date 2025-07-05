ce9fca2080cbcf279c035750d6abe5b20b564b26
Kartik B H <95575463+kartikbh56@users.noreply.github.com>
2025-07-05 18:46:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -93,3 +93,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelector('.items');
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    items.addEventListener('mousedown', (e) => {
+        isDown = true;
+        items.classList.add('active');
+        startX = e.pageX - items.offsetLeft;
+        scrollLeft = items.scrollLeft;
+    });
+
+    items.addEventListener('mouseleave', () => {
+        isDown = false;
+        items.classList.remove('active');
+    });
+
+    items.addEventListener('mouseup', () => {
+        isDown = false;
+        items.classList.remove('active');
+    });
+
+    items.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - items.offsetLeft;
+        const walk = (x - startX); // You can multiply for speed
+        items.scrollLeft = scrollLeft - walk;
+    });
+});
